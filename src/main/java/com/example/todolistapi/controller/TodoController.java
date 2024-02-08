a252f8799abfdec82674d26d7afa5979c32740c6
TheAbraaonix <carlosabraao@hotmail.com.br>
2024-02-08 13:08:06 -0300

    implementando o controller todo

---
+++ src/main/java/com/example/todolistapi/controller/TodoController.java
@@ -0,0 +1,45 @@
+package com.example.todolistapi.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todolistapi.entity.Todo;
+import com.example.todolistapi.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") long id) {
+        return todoService.delete(id);
+    }
+}
